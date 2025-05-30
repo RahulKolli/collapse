d0b40ba7a7dc1e1797da8e4b4c68c3f68a52b3f2
Rahul Kolli <rahulkollit@gmail.com>
2025-05-30 16:04:36 +0530

    Rahul almost Finished

--- src/app/admin/gigs/page.jsx
+++ src/app/admin/gigs/page.jsx
@@ -1,9 +1,13 @@
+import { ChartAreaInteractive } from "@/components/chart-area-interactive";
+
 export default function Page() {
   return (
-    <main className="flex min-h-screen items-center justify-center bg-gray-100 p-8">
-      <div className="text-center">
-        <h1 className="text-3xl font-bold text-gray-800">admin/gigs</h1>
-      </div>
-    </main>
+    <>
+    
+    
+    < ChartAreaInteractive />
+
+</>
+    
   );
 }
